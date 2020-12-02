d050bd4aa297a00f5061ac1f0265776d3e8531d9
extasy44 <seoheejun@gmail.com>
2020-12-02 16:50:47 +1100

    handling css

--- more-complex-app/marketing/src/App.js
+++ more-complex-app/marketing/src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
-const App = () => {
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
+export default () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
-            <Route exact path="/" component={Landing} />
+            <Route path="/" component={Landing} />
           </Switch>
         </BrowserRouter>
       </StylesProvider>
     </div>
   );
 };
-
-export default App;
