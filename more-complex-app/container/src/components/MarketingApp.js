1679af739c435655b930a8a3fb1cfdf2a4e46416
extasy44 <seoheejun@gmail.com>
2020-12-03 23:55:02 +1100

    Implementing multi-tier navigation, BrowseHistory and MemoryHistory, Communitation between apps onNavigate

--- more-complex-app/container/src/components/MarketingApp.js
+++ more-complex-app/container/src/components/MarketingApp.js
@@ -1,14 +1,26 @@
 import { mount } from 'marketing/MarketingApp';
 import React, { useRef, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const MarketingApp = () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        console.log(pathname);
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+    console.log(onParentNavigate);
+    history.listen(onParentNavigate);
+  }, []);
 
-  return <div ref={ref}></div>;
+  return <div ref={ref} />;
 };
 
 export default MarketingApp;
